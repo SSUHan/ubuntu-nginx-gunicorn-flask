b56c04881211d12fbb6663008601ef879605f206
이준수 <ljs93kr@gmail.com>
2016-07-13 17:31:57 +0900

    orm database.py define

--- database.py
+++ database.py
@@ -5,4 +5,11 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('mysql://root:mysql-root@localhost/test?charset=utf8', convert_unicode=False)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+	import models
+	Base.metadata.create_all(engine)
